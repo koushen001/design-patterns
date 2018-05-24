17f09f6485093869f43845002b8bbf3fa59fc8be
kouliang <ex-koul001@cmrh.com>
2018-05-24 17:01:28 +0800

    初始化

---
+++ src/main/java/com/cike/Test.java
@@ -0,0 +1,10 @@
+package com.cike;
+
+/**
+ * ${DESCRIPTION}
+ *
+ * @author kouliang
+ * @create 2018-05-24 17:00
+ **/
+public class Test {
+}
---
+++ src/main/java/com/xp/Test.java
@@ -0,0 +1,10 @@
+package com.xp;
+
+/**
+ * ${DESCRIPTION}
+ *
+ * @author kouliang
+ * @create 2018-05-24 17:01
+ **/
+public class Test {
+}
